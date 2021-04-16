8508ff357ba956b01b101e65f63220ecb45a8313
IzumiSakai1995 <40541052+IzumiSakai1995@users.noreply.github.com>
2021-04-17 05:23:44 +0800

    Initial commit

---
+++ rh-api/src/main/java/com/lz/rhapi/serivce/UserService.java
@@ -0,0 +1,16 @@
+package com.lz.rhapi.serivce;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 用户接口
+ */
+
+@Component
+public interface UserService {
+    /**
+     * 接口测试
+     * @return
+     */
+    Integer returnTest();
+}
---
+++ rh-provider/src/main/java/com/lz/rhprovider/Service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.lz.rhprovider.Service;
+
+import com.lz.rhapi.serivce.UserService;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.stereotype.Component;
+
+@DubboService(version = "1.0.0")
+@Component
+public class UserServiceImpl implements UserService {
+    @Override
+    public Integer returnTest() {
+        return 1;
+    }
+}
