53cd4041514687fc362d3399204819826e573691
IzumiSakai1995 <40541052+IzumiSakai1995@users.noreply.github.com>
2021-04-18 18:14:31 +0800

    kafka

---
+++ rh-kafka/src/main/java/com/lz/rhkafka/RhKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.lz.rhkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhKafkaApplication.class, args);
+	}
+
+}
