f72acc760c4686c5b50bae1a2d920c3565e0bd9a
IzumiSakai1995 <40541052+IzumiSakai1995@users.noreply.github.com>
2021-04-20 03:41:04 +0800

    dockerfile for provider

---
+++ rh-provider/src/main/java/com/lz/rhprovider/Service/UserService.java
@@ -0,0 +1,17 @@
+package com.lz.rhprovider.Service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 用户接口
+ */
+
+@Component
+public interface UserService {
+    /**
+     * 接口测试
+     * @return
+     */
+    Integer returnTest();
+
+}
--- rh-provider/src/main/java/com/lz/rhprovider/Service/UserServiceImpl.java
+++ rh-provider/src/main/java/com/lz/rhprovider/Service/UserServiceImpl.java
@@ -1,20 +1,13 @@
 package com.lz.rhprovider.Service;
 
-import com.lz.rhapi.pojo.Res;
-import com.lz.rhapi.serivce.UserService;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.stereotype.Component;
 
 @DubboService(version = "1.0.0")
 @Component
-public class UserServiceImpl implements UserService {
-    @Override
+public class UserServiceImpl implements UserService{
     public Integer returnTest() {
         return 1;
     }
 
-    @Override
-    public Res userRegistry() {
-        return null;
-    }
 }
