2e2f91ebf765dfcf60fd907094985535d914c357
IzumiSakai1995 <40541052+IzumiSakai1995@users.noreply.github.com>
2021-04-21 03:30:55 +0800

    github action push调试

--- rh-provider/src/main/java/com/lz/rhprovider/Service/UserServiceImpl.java
+++ rh-provider/src/main/java/com/lz/rhprovider/Service/UserServiceImpl.java
@@ -1,13 +1,20 @@
 package com.lz.rhprovider.Service;
 
+import com.lz.rhapi.pojo.Res;
+import com.lz.rhapi.serivce.UserService;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.stereotype.Component;
 
 @DubboService(version = "1.0.0")
 @Component
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+    @Override
     public Integer returnTest() {
         return 1;
     }
 
+    @Override
+    public Res userRegistry() {
+        return null;
+    }
 }
