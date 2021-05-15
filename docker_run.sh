9fe6e84850ffcc5c3c695ea2a5be90508302686d
IzumiSakai1995 <40541052+IzumiSakai1995@users.noreply.github.com>
2021-05-15 16:27:51 +0800

    容器化部署准备

--- docker_run.sh
+++ docker_run.sh
@@ -5,7 +5,8 @@ do
   done
 
 ls
-docker build -f ./rh-provider/Dockerfile -t user-provider .
+docker build -f ./user-provider/Dockerfile -t user-provider .
+docker build -f ./user-consumer/Dockerfile -t user-consumer .
 docker login --username=奈文摩尔_1995 --password=950519lz registry.cn-hangzhou.aliyuncs.com
 docker tag 79800c567b02 registry.cn-hangzhou.aliyuncs.com/private_proj/user-provider:1.0.0
 docker push registry.cn-hangzhou.aliyuncs.com/private_proj/user-provider:1.0.0
